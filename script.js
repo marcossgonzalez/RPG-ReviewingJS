35f7cfc1876d86f4ee5954dd0f1039187ca79ab0
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2024-04-24 18:05:22 -0300

    Finished game
    finished game. now you can play.

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const locations = [{
 {
   name: "kill monster",
   "button text": ["Go to town square", "Go to town square", "Go to town square"],
-  "button functions": [goTown, goTown, goTown],
+  "button functions": [goTown, goTown, easterEgg],
   text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
 },
 {
@@ -259,6 +259,9 @@ function pick(guess) {
     text.innerText += "Wrong! You lose 10 health!";
     health -= 10;
     healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
   }
 }
 function pickTwo() {
